aee7f12d39cdd8e2dc5ce1bdaf6ffab2c7e75f6d
Mohamed Salih <Mohamed.Salih.Dev@gmail.com>
2020-10-29 21:46:28 -0700

    added navbar and login components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import navBar from "./component/pages/navBar";
+import navBar from "./component/navBar";
 import "../public/css/style";
+import Login from "./component/pages/login"
 
 function App() {
   return (
     <Router>
       <div>
+        <Login />
         <navBar />
         <Route exact path="/" component={Home} />
         <Footer />
