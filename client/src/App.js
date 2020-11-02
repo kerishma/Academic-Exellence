ab7237d664dd5071aec7e8b04f9ebe981daa04ab
kerishma tarin <kerishma.tarin@gmail.com>
2020-11-01 23:16:03 -0700

    fixed header,fooder, flashspeech and main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import Main from "./pages/main";
-import "../src/css/style.css";
-import Login from "./pages/login"
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Main from './pages/main';
+import '../src/css/style.css';
+import Login from './pages/login';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import FlashSpeech from './components/flashSpeech/index';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
     <Router>
       <div>
+        <Header />
         <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/main" component={Main} />
-        {/* <Footer /> */}
+          <Route exact path="/" component={Login} />
+          <Route exact path="/main" component={Main} />
         </Switch>
+        <Footer />
       </div>
     </Router>
   );
---
+++ client/src/component/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../css/style.css';
+
+function Header() {
+  return (
+    <div className="jumbotron text-center">
+      <div className="container text-light">
+        <h1>click on something</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
