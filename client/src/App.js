ede1817b4a644e6ad92e3f02b600b431e1816f48
Daniel Altman <Daniel.Altman4@gmail.com>
2020-11-04 00:03:09 -0700

    adding signup page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import main from './pages/main';
 import '../src/css/style.css';
 import Login from './pages/login';
+import Signup from './pages/signup';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Navbar from "./components/navBar/Navbar"
@@ -23,6 +24,7 @@ function App() {
         {/* <Header /> */}
         <Switch>
           <Route exact path="/" component={Login} />
+          <Route exact path="/signup" component={Signup} />
           <Route exact path="/main" component={main} />
         </Switch>
         <Footer />
