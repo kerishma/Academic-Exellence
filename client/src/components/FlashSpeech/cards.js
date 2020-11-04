7b4758b18227c903e1d59f9d97c589f688dc0a85
Anubis <desertdog1096@yahoo.com>
2020-11-03 19:41:02 -0700

    add cards js and css files to FlashSpeech folder

---
+++ client/src/components/FlashSpeech/cards.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flashcard Test</title>
+    <link rel="stylesheet" href="cards.css" />
+</head>
+<body>
+
+    <div class="mainContainer">
+
+        <div class="card">
+  
+          <div class="cardFront"><h1>Front of Card</h1><p>This side can hold the word from the array.<br><br><br>Touch (not click) card with mouse to turn card over.</p>
+            <button>All caps</button>
+            <button>Lower case</button></div>
+  
+          <div class="cardBack"><h1>Back of Card</h1><p>This side can hold an image and also the voice button.</p>
+          <button>Say it!</button></div>
+  
+        </div>
+      </div>
+    
+</body>
+</html>
