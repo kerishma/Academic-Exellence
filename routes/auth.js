5f00c8e0bec060aba99d789e78223dfaa80a5c73
Mohamed Salih <Mohamed.Salih.Dev@gmail.com>
2020-11-04 17:37:55 -0700

    added password hashing code

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
+const bcrypt = require("bcrypt.js");
 
 router.get("/", (req, res) => {
     res.send("hello")
@@ -17,21 +18,27 @@ router.post("/signup", (req, res) => {
         if (savedUser) {
             return res.status(422).json({ error: "email already exists." })
         }
-        const user = new User({
-            email: email,
-            password: password
-        })
-        user.save()
-            .then(user => {
-                res.json({ message: "saved successfully" })
-            })
-            .catch(err => {
-                console.log("error saving user")
-            })
-    }
+        bcrypt.hash(password, 12)
+            .then(hashedpassword => {
+                const user = new User({
+                    email: email,
+                    password: password
+                })
+                user.save()
+                    .then(user => {
+                        res.json({ message: "saved successfully" })
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
 
-    )
+            })
+
+    }).catch(err => {
+        console.log(err)
+    })
 
 })
+
 
 module.exports = router
